262d51e2070ead9d692ea218e0e8c971414e225d
Rogério Alonso <69876702+rogerioloualonso@users.noreply.github.com>
2023-09-27 20:16:44 -0300

    Spring Initializr

---
+++ src/main/java/br/com/sgai/SgaiApplication.java
@@ -0,0 +1,13 @@
+package br.com.sgai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sgai/SgaiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sgai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
