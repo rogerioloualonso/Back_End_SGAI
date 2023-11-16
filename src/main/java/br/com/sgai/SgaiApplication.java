6dcdd898aac229a55ce2b7bfcf232698adf5a55e
Rogério Alonso <69876702+rogerioloualonso@users.noreply.github.com>
2023-11-16 17:02:10 -0300

    Inclusão de anotação na classe da aplicação

--- src/main/java/br/com/sgai/SgaiApplication.java
+++ src/main/java/br/com/sgai/SgaiApplication.java
@@ -2,7 +2,9 @@ package br.com.sgai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class SgaiApplication {
 
