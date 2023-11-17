6c69acf75c2a308efc2b352a64694b5cc3de1989
Rogério Alonso <69876702+rogerioloualonso@users.noreply.github.com>
2023-11-17 13:30:30 -0300

    Atualização do cors

--- src/main/java/br/com/sgai/config/CorsConfig.java
+++ src/main/java/br/com/sgai/config/CorsConfig.java
@@ -3,11 +3,13 @@ package br.com.sgai.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfig {
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
 
     @SuppressWarnings("deprecation")
 	@Bean
@@ -16,7 +18,7 @@ public class CorsConfig {
         	
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/auth/discente")
+                registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
